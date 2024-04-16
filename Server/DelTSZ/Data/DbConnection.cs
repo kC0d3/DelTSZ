c67460ece9fd260dd0418ddc32c09501a1377f04
kC0d3 <kC0d3@github.com>
2024-04-16 11:54:58 +0200

    feat: Add GetDockerConnectionString method

--- Server/DelTSZ/Data/DbConnection.cs
+++ Server/DelTSZ/Data/DbConnection.cs
@@ -1,5 +1,15 @@
+using DotNetEnv;
+
 namespace DelTSZ.Data;
 
 public static class DbConnection
 {
+    public static string GetDockerConnectionString()
+    {
+        var root = Directory.GetCurrentDirectory();
+        var dotenv = Path.Combine(root, "..", "..", ".env");
+        Env.Load(dotenv);
+        return
+            $"Server={Environment.GetEnvironmentVariable("DBHOST")},{Environment.GetEnvironmentVariable("DBPORT")};Database={Environment.GetEnvironmentVariable("DBNAME")};User Id={Environment.GetEnvironmentVariable("DBUSER")};Password={Environment.GetEnvironmentVariable("DBPASSWORD")};Encrypt=false;";
+    }
 }
