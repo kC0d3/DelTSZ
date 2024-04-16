4bb9c36ec3dd27944733d6b91699b5327748e7dc
kC0d3 <kC0d3@github.com>
2024-04-16 11:56:43 +0200

    feat: Add DbContext service and connection string

--- Server/DelTSZ/Program.cs
+++ Server/DelTSZ/Program.cs
@@ -1,9 +1,13 @@
+using DelTSZ.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = DbConnection.GetDockerConnectionString();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+AddDbContext();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -16,3 +20,11 @@ app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
+
+//Application methods
+
+void AddDbContext()
+{
+    builder.Services.AddDbContext<DataContext>(optionsBuilder =>
+        optionsBuilder.UseSqlServer(connectionString));
+}
