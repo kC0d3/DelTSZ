c849ee335e241615fe89b5aae7fe36deeb710fd0
kC0d3 <kC0d3@github.com>
2024-04-16 11:55:42 +0200

    feat: Add GetTestConnectionString method

--- Server/DelTSZ/Data/DbConnection.cs
+++ Server/DelTSZ/Data/DbConnection.cs
@@ -12,4 +12,13 @@ public static class DbConnection
         return
             $"Server={Environment.GetEnvironmentVariable("DBHOST")},{Environment.GetEnvironmentVariable("DBPORT")};Database={Environment.GetEnvironmentVariable("DBNAME")};User Id={Environment.GetEnvironmentVariable("DBUSER")};Password={Environment.GetEnvironmentVariable("DBPASSWORD")};Encrypt=false;";
     }
+    
+    public static string GetTestConnectionString()
+    {
+        var root = Directory.GetCurrentDirectory();
+        var dotenv = Path.Combine(root, "..", "..", "..", "..", "..", ".env");
+        Env.Load(dotenv);
+        return
+            $"Server={Environment.GetEnvironmentVariable("DBHOST")},{Environment.GetEnvironmentVariable("DBPORT")};Database={Environment.GetEnvironmentVariable("TESTDBNAME")};User Id={Environment.GetEnvironmentVariable("DBUSER")};Password={Environment.GetEnvironmentVariable("DBPASSWORD")};Encrypt=false;";
+    }
 }
