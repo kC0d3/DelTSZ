871b9a46eab367ab8f7d29f4354ce6d15f3c8bac
kC0d3 <kC0d3@github.com>
2024-04-16 11:47:09 +0200

    feat: Add ProductType enum implementation

---
+++ Server/DelTSZ/Models/Enums/ProductType.cs
@@ -0,0 +1,15 @@
+namespace DelTSZ.Models.Enums;
+
+public enum ProductType
+{
+    Paprika,
+    Tomato,
+    Radish,
+    Cucumber,
+    Cabbage,
+    Potato,
+    Zucchini,
+    Eggplant,
+    Mushroom,
+    Onion,
+}
