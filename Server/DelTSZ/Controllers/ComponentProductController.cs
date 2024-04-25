2694c9959b34cb872499d4a64ba20aaf221e299e
kC0d3 <kC0d3@github.com>
2024-04-25 09:18:20 +0200

    feat: Add ComponentProductController and GetAllOwnerSingleProducts API endpoints

---
+++ Server/DelTSZ/Controllers/ComponentProductController.cs
@@ -0,0 +1,24 @@
+﻿using DelTSZ.Models.Products.ComponentProducts;
+using DelTSZ.Repositories.ComponentProductRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DelTSZ.Controllers;
+
+[Route("api/component-products")]
+[ApiController]
+public class ComponentProductController(IComponentProductRepository componentProductRepository) : ControllerBase
+{
+    [HttpGet, Authorize(Roles = "Costumer")]
+    public async Task<ActionResult<IEnumerable<ComponentProductRequest>>> GetAllOwnerSingleProducts()
+    {
+        try
+        {
+            return Ok(await componentProductRepository.GetAllOwnerSingleProducts());
+        }
+        catch (Exception)
+        {
+            return NotFound("Error getting products.");
+        }
+    }
+}
