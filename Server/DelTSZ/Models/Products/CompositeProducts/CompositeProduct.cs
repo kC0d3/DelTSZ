2f977bd37d1330d066ad8bca827e68b7d41c3692
kC0d3 <kC0d3@github.com>
2024-04-25 09:08:01 +0200

    feat: Add CompositeProduct model class

---
+++ Server/DelTSZ/Models/Products/CompositeProducts/CompositeProduct.cs
@@ -0,0 +1,19 @@
+﻿using DelTSZ.Models.Enums;
+using DelTSZ.Models.Products.ComponentProducts;
+using DelTSZ.Models.Users;
+
+namespace DelTSZ.Models.Products.CompositeProducts;
+
+public class CompositeProduct : IProduct
+{
+    //Properties
+    public int Id { get; init; }
+    public CompositeProductType ProductType { get; init; }
+    public DateTime Packed { get; init; }
+    public int Amount { get; set; }
+    public ICollection<ComponentProduct>? Components { get; set; }
+
+    //Navigation properties
+    public string? UserId { get; init; }
+    public User? User { get; init; }
+}
