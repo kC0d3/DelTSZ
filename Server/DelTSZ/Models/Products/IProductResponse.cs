a22b275fffdaa5dd1764d99160a7cbf16979a592
kC0d3 <kC0d3@github.com>
2024-04-25 09:07:03 +0200

    refactor: Refactor ProductResponse class to IProductResponse interface

---
+++ Server/DelTSZ/Models/Products/IProductResponse.cs
@@ -0,0 +1,9 @@
+﻿using DelTSZ.Models.Enums;
+
+namespace DelTSZ.Models.Products;
+
+public interface IProductResponse
+{
+    public int Id { get; init; }
+    public string? UserId { get; init; }
+}
