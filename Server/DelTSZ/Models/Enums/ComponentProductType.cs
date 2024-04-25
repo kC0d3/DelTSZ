98b3057473c4bfdea6104d99e82ce1ccc62a9e50
kC0d3 <kC0d3@github.com>
2024-04-25 09:03:48 +0200

    refactor: Refactor ProductType enum to ComponentProductType enum

---
+++ Server/DelTSZ/Models/Enums/ComponentProductType.cs
@@ -0,0 +1,15 @@
+﻿namespace DelTSZ.Models.Enums;
+
+public enum ComponentProductType
+{
+    Cabbage,
+    Celery,
+    Cucumber,
+    Mushroom,
+    Onion,
+    ParsleyRoot,
+    Paprika,
+    Potato,
+    Radish,
+    Tomato,
+}
