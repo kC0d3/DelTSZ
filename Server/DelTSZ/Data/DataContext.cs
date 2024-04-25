9fcce12c1306803d92b2a17d19a9967b19094cc3
kC0d3 <kC0d3@github.com>
2024-04-25 09:11:27 +0200

    refactor: Refactor DataContext to implement new product classes

--- Server/DelTSZ/Data/DataContext.cs
+++ Server/DelTSZ/Data/DataContext.cs
@@ -1,5 +1,6 @@
 using DelTSZ.Models.Addresses;
-using DelTSZ.Models.Products;
+using DelTSZ.Models.Products.ComponentProducts;
+using DelTSZ.Models.Products.CompositeProducts;
 using DelTSZ.Models.Users;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -9,7 +10,8 @@ namespace DelTSZ.Data;
 
 public class DataContext : IdentityDbContext<User, IdentityRole, string>
 {
-    public DbSet<Product>? Products { get; set; }
+    public DbSet<ComponentProduct>? ComponentProducts { get; set; }
+    public DbSet<CompositeProduct>? CompositeProducts { get; set; }
     public DbSet<Address>? Addresses { get; set; }
 
     public DataContext(DbContextOptions<DataContext> options)
@@ -21,12 +23,21 @@ public class DataContext : IdentityDbContext<User, IdentityRole, string>
     {
         base.OnModelCreating(builder);
 
-        builder.Entity<Product>()
+        builder.Entity<ComponentProduct>()
             .HasOne(p => p.User)
-            .WithMany(u => u.Products)
+            .WithMany(u => u.ComponentProducts)
             .HasForeignKey(p => p.UserId)
             .OnDelete(DeleteBehavior.Cascade);
 
+        builder.Entity<CompositeProduct>()
+            .HasOne(p => p.User)
+            .WithMany(u => u.CompositeProducts)
+            .HasForeignKey(p => p.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<CompositeProduct>()
+            .HasMany(p => p.Components);
+        
         builder.Entity<User>()
             .HasOne(u => u.Address)
             .WithOne(a => a.User)
@@ -34,7 +45,13 @@ public class DataContext : IdentityDbContext<User, IdentityRole, string>
             .OnDelete(DeleteBehavior.Cascade);
 
         builder.Entity<User>()
-            .HasMany(u => u.Products)
+            .HasMany(u => u.ComponentProducts)
+            .WithOne(p => p.User)
+            .HasForeignKey(p => p.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<User>()
+            .HasMany(u => u.CompositeProducts)
             .WithOne(p => p.User)
             .HasForeignKey(p => p.UserId)
             .OnDelete(DeleteBehavior.Cascade);
