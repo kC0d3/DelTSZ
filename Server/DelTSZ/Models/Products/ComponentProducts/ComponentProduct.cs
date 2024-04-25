b2b0ccfe710ae1f998070e4973a022cd3901ae52
kC0d3 <kC0d3@github.com>
2024-04-25 09:04:51 +0200

    feat: Add ComponentProduct model class

---
+++ Server/DelTSZ/Models/Products/ComponentProducts/ComponentProduct.cs
@@ -0,0 +1,19 @@
+﻿using DelTSZ.Models.Enums;
+using DelTSZ.Models.Users;
+
+namespace DelTSZ.Models.Products.ComponentProducts;
+
+public class ComponentProduct : IProduct
+{
+    //Properties
+    
+    public int Id { get; init; }
+    public ComponentProductType ProductType { get; init; }
+    public DateTime Received { get; init; }
+    public double Amount { get; set; }
+    
+    //Navigation properties
+    
+    public string? UserId { get; init; }
+    public User? User { get; init; }
+}
