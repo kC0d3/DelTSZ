646beb12f79ac20d6309f02421d292fb7eab8940
kC0d3 <kC0d3@github.com>
2024-04-25 09:05:31 +0200

    feat: Add ComponentProductRequest model class

---
+++ Server/DelTSZ/Models/Products/ComponentProducts/ComponentProductRequest.cs
@@ -0,0 +1,10 @@
+﻿using DelTSZ.Models.Enums;
+
+namespace DelTSZ.Models.Products.ComponentProducts;
+
+public class ComponentProductRequest
+{
+    public ComponentProductType ProductType { get; init; }
+    public DateTime Received { get; init; }
+    public double Amount { get; set; }
+}
