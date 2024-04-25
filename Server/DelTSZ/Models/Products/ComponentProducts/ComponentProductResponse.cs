cf24c58bf30b9ea1db3045834c8ca8368e8a9424
kC0d3 <kC0d3@github.com>
2024-04-25 09:05:49 +0200

    feat: Add ComponentProductResponse model class

---
+++ Server/DelTSZ/Models/Products/ComponentProducts/ComponentProductResponse.cs
@@ -0,0 +1,12 @@
+﻿using DelTSZ.Models.Enums;
+
+namespace DelTSZ.Models.Products.ComponentProducts;
+
+public class ComponentProductResponse : IProductResponse
+{
+    public int Id { get; init; }
+    public ComponentProductType ProductType { get; init; }
+    public DateTime Received { get; init; }
+    public double Amount { get; init; }
+    public string? UserId { get; init; }
+}
