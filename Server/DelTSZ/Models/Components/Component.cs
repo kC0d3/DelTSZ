59e30199cd67d55913cfa5e3166cf888519e9c17
kC0d3 <kC0d3@github.com>
2024-04-29 17:32:16 +0200

    refactor: Refactor Component from ComponentProduct

--- Server/DelTSZ/Models/Components/Component.cs
+++ Server/DelTSZ/Models/Components/Component.cs
@@ -1,19 +1,22 @@
 ﻿using DelTSZ.Models.Enums;
+using DelTSZ.Models.Products;
 using DelTSZ.Models.Users;
 
-namespace DelTSZ.Models.Products.ComponentProducts;
+namespace DelTSZ.Models.Components;
 
-public class ComponentProduct : IProduct
+public class Component
 {
     //Properties
-    
+
     public int Id { get; init; }
-    public ComponentProductType ProductType { get; init; }
+    public ComponentType Type { get; init; }
     public DateTime Received { get; init; }
     public double Amount { get; set; }
-    
+
     //Navigation properties
-    
+
     public string? UserId { get; init; }
     public User? User { get; init; }
+    public int? ProductId { get; init; }
+    public Product? Product { get; init; }
 }
