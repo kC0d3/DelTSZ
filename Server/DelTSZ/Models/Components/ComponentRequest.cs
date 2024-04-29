b4d30ed5ea8275b17b6308eb5ccaecf5518fb371
kC0d3 <kC0d3@github.com>
2024-04-29 17:37:13 +0200

    refactor: Refactor ComponentProductRequest to ComponentRequest

---
+++ Server/DelTSZ/Models/Components/ComponentRequest.cs
@@ -0,0 +1,10 @@
+﻿using DelTSZ.Models.Enums;
+
+namespace DelTSZ.Models.Components;
+
+public class ComponentRequest
+{
+    public ComponentType Type { get; init; }
+    public DateTime Received { get; init; }
+    public double Amount { get; set; }
+}
