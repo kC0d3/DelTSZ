7e214d965ae7756734c154732ed5671497f3f5cb
kC0d3 <kC0d3@github.com>
2024-04-29 21:09:57 +0200

    feat: Add ProductTypeExtensions to ProductType

--- Server/DelTSZ/Models/Enums/ProductType.cs
+++ Server/DelTSZ/Models/Enums/ProductType.cs
@@ -8,3 +8,57 @@ public enum ProductType
     RatatouilleMix500G,
     SoupMix750G,
 }
+
+public static class ProductTypeExtensions
+{
+    private static readonly
+        Dictionary<ProductType, ICollection<(double Amount, ComponentType Component)>>
+        ProductMap =
+            new()
+            {
+                {
+                    ProductType.Paprika400G,
+                    new List<(double, ComponentType)>
+                    {
+                        (0.4, ComponentType.Paprika)
+                    }
+                },
+                {
+                    ProductType.Tomato200G,
+                    new List<(double, ComponentType)>
+                    {
+                        (0.2, ComponentType.Tomato)
+                    }
+                },
+                {
+                    ProductType.Tomato500G,
+                    new List<(double, ComponentType)>
+                    {
+                        (0.5, ComponentType.Tomato)
+                    }
+                },
+                {
+                    ProductType.RatatouilleMix500G,
+                    new List<(double, ComponentType)>
+                    {
+                        (0.25, ComponentType.Paprika),
+                        (0.25, ComponentType.Tomato)
+                    }
+                },
+                {
+                    ProductType.SoupMix750G,
+                    new List<(double, ComponentType)>
+                    {
+                        (0.25, ComponentType.Carrot),
+                        (0.25, ComponentType.ParsleyRoot),
+                        (0.15, ComponentType.Celery),
+                        (0.1, ComponentType.Onion)
+                    }
+                },
+            };
+
+    public static IEnumerable<(double, ComponentType)> GetComponentsAmount(this ProductType type)
+    {
+        return ProductMap[type];
+    }
+}
