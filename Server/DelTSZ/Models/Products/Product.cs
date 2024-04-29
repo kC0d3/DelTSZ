718da78ade7446162fd639964bf30e5ae601fd4e
kC0d3 <kC0d3@github.com>
2024-04-29 17:34:35 +0200

    refactor: Refactor Product from CompositeProduct

--- Server/DelTSZ/Models/Products/Product.cs
+++ Server/DelTSZ/Models/Products/Product.cs
@@ -1,12 +1,17 @@
+﻿using DelTSZ.Models.Components;
 using DelTSZ.Models.Enums;
 using DelTSZ.Models.Users;
 
 namespace DelTSZ.Models.Products;
 
-public interface IProduct
+public class Product
 {
     //Properties
     public int Id { get; init; }
+    public ProductType Type { get; init; }
+    public DateTime Packed { get; init; }
+    public int Amount { get; set; }
+    public ICollection<Component>? Components { get; init; }
 
     //Navigation properties
     public string? UserId { get; init; }
