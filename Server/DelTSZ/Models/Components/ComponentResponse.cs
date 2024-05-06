33e6c37f3059a7bde3206b66886ef161c17bb760
kC0d3 <kC0d3@github.com>
2024-05-06 11:54:19 +0200

    refactor: Change Amount type

--- Server/DelTSZ/Models/Components/ComponentResponse.cs
+++ Server/DelTSZ/Models/Components/ComponentResponse.cs
@@ -8,6 +8,5 @@ public class ComponentResponse
     public int Id { get; init; }
     public ComponentType Type { get; init; }
     public DateTime Received { get; init; }
-    public double Amount { get; init; }
-    public string? UserId { get; init; }
+    public decimal Amount { get; init; }
 }
