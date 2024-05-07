d7eb490bd93c04d1df67765a3af873bd4089391d
kC0d3 <kC0d3@github.com>
2024-05-07 13:31:20 +0200

    feat: Add ComponentUpdateRequest model class

---
+++ Server/DelTSZ/Models/Components/ComponentUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿using DelTSZ.Models.Enums;
+
+namespace DelTSZ.Models.Components;
+
+public class ComponentUpdateRequest
+{
+    public ComponentType Type { get; init; }
+    public DateTime Received { get; init; }
+    public decimal Amount { get; init; }
+}
