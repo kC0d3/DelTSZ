ea737b901193a68de6917bfd9b24b693d2c925fa
kC0d3 <kC0d3@github.com>
2024-05-07 14:21:21 +0200

    feat: Add ProductController and GetAllOwnerProducts API endpoint

---
+++ Server/DelTSZ/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using DelTSZ.Models.Components;
+using DelTSZ.Repositories.ProductRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DelTSZ.Controllers;
+
+[Route("api/products")]
+[ApiController]
+public class ProductController(IProductRepository productRepository) : ControllerBase
+{
+    [HttpGet, Authorize(Roles = "Costumer")]
+    public async Task<ActionResult<IEnumerable<ComponentRequest>>> GetAllOwnerProducts()
+    {
+        try
+        {
+            return Ok(await productRepository.GetAllOwnerProducts());
+        }
+        catch (Exception)
+        {
+            return NotFound("Error getting products.");
+        }
+    }
+}
