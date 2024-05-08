494bc17d8a42814a43c798162762fafa38a5fa3d
kC0d3 <kC0d3@github.com>
2024-05-08 13:43:59 +0200

    feat: Add ComponentSumResponse model class

---
+++ Server/DelTSZ/Models/Components/ComponentSumResponse.cs
@@ -0,0 +1,9 @@
+﻿using DelTSZ.Models.Enums;
+
+namespace DelTSZ.Models.Components;
+
+public class ComponentSumResponse
+{
+    public ComponentType Type { get; init; }
+    public decimal Amount { get; init; }
+}
