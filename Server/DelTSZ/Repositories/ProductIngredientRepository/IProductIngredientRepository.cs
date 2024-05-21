a2f8509828da1dded69c105c3542ae7dd2c74deb
kC0d3 <kC0d3@github.com>
2024-05-21 10:37:38 +0200

    feat: Add ProductIngredientRepository, interface and CreateProductIngredients method

---
+++ Server/DelTSZ/Repositories/ProductIngredientRepository/IProductIngredientRepository.cs
@@ -0,0 +1,9 @@
+﻿using DelTSZ.Models.ProductIngredients;
+using DelTSZ.Models.Products;
+
+namespace DelTSZ.Repositories.ProductIngredientRepository;
+
+public interface IProductIngredientRepository
+{
+    Task<List<ProductIngredient>> CreateProductIngredients(ProductRequest productRequest);
+}
---
+++ Server/DelTSZ/Repositories/ProductIngredientRepository/ProductIngredientRepository.cs
@@ -0,0 +1,75 @@
+﻿using DelTSZ.Data;
+using DelTSZ.Models.Enums;
+using DelTSZ.Models.ProductIngredients;
+using DelTSZ.Models.Products;
+using DelTSZ.Repositories.IngredientRepository;
+
+namespace DelTSZ.Repositories.ProductIngredientRepository;
+
+public class ProductIngredientRepository(DataContext dataContext, IIngredientRepository ingredientRepository)
+    : IProductIngredientRepository
+{
+    public async Task<List<ProductIngredient>> CreateProductIngredients(ProductRequest productRequest)
+    {
+        var productDetails = productRequest.Type.GetProductDetails().ToList();
+        var ingredients = new List<ProductIngredient>();
+
+        foreach (var pd in productDetails)
+        {
+            var demandAmount = pd.Item1 * productRequest.Amount;
+            var ingredientAmount = 0m;
+
+            do
+            {
+                var oldestIngredient = await ingredientRepository.GetOwnerOldestIngredientByType(pd.Item2);
+                if (oldestIngredient!.Amount - pd.Item1 <= 0)
+                {
+                    if (ingredients.Exists(pi =>
+                            pi.Received == oldestIngredient.Received && pi.Type == oldestIngredient.Type))
+                    {
+                        ingredients.FirstOrDefault(pi =>
+                                pi.Received == oldestIngredient.Received && pi.Type == oldestIngredient.Type)!
+                            .Amount += pd.Item1;
+                    }
+                    else
+                    {
+                        ingredients.Add(new ProductIngredient
+                        {
+                            Amount = oldestIngredient.Amount,
+                            Received = oldestIngredient.Received,
+                            Type = oldestIngredient.Type
+                        });
+                    }
+
+                    ingredientAmount += oldestIngredient.Amount;
+                    await ingredientRepository.DeleteIngredient(oldestIngredient);
+                }
+                else
+                {
+                    if (ingredients.Exists(pi =>
+                            pi.Received == oldestIngredient.Received && pi.Type == oldestIngredient.Type))
+                    {
+                        ingredients.FirstOrDefault(pi =>
+                                pi.Received == oldestIngredient.Received && pi.Type == oldestIngredient.Type)!
+                            .Amount += pd.Item1;
+                    }
+                    else
+                    {
+                        ingredients.Add(new ProductIngredient
+                        {
+                            Amount = pd.Item1,
+                            Received = oldestIngredient.Received,
+                            Type = oldestIngredient.Type
+                        });
+                    }
+
+                    ingredientAmount += pd.Item1;
+                    oldestIngredient.Amount -= pd.Item1;
+                    await ingredientRepository.UpdateIngredient(oldestIngredient);
+                }
+            } while (ingredientAmount < demandAmount);
+        }
+
+        return ingredients;
+    }
+}
