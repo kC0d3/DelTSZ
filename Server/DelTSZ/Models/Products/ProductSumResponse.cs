9ccf9bb8779b01f041c00008b39b615c3b642571
kC0d3 <kC0d3@github.com>
2024-05-22 09:32:02 +0200

    feat: Add ProductSumResponse model class

---
+++ Server/DelTSZ/Models/Products/ProductSumResponse.cs
@@ -0,0 +1,9 @@
+﻿using DelTSZ.Models.Enums;
+
+namespace DelTSZ.Models.Products;
+
+public class ProductSumResponse
+{
+    public ProductType Type { get; init; }
+    public int Amount { get; init; }
+}
