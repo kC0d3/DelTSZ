46d36d9c855a3b1d96f2e521e0fd8f2eee5ce9a1
kC0d3 <kC0d3@github.com>
2024-05-23 09:12:30 +0200

    feat: Add AddressResponse model class

---
+++ Server/DelTSZ/Models/Addresses/AddressResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace DelTSZ.Models.Addresses;
+
+public class AddressResponse
+{
+    public string? Zipcode { get; set; }
+    public string? City { get; set; }
+    public string? Street { get; set; }
+    public string? Housenumber { get; set; }
+}
