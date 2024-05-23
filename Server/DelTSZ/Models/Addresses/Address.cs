a3d16a35f5046276dbdf0bde54acfd6117c847f1
kC0d3 <kC0d3@github.com>
2024-05-23 09:01:15 +0200

    fix: Add StringLength to prevent unlimited string length warning

--- Server/DelTSZ/Models/Addresses/Address.cs
+++ Server/DelTSZ/Models/Addresses/Address.cs
@@ -1,16 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 using DelTSZ.Models.Users;
 
 namespace DelTSZ.Models.Addresses;
+
 public class Address
 {
     //Properties
     public int Id { get; init; }
-    public string? ZipCode { get; set; }
-    public string? City { get; set; }
-    public string? Street { get; set; }
-    public string? HouseNumber { get; set; }
+    [StringLength(10)] public string? ZipCode { get; set; }
+    [StringLength(50)] public string? City { get; set; }
+    [StringLength(100)] public string? Street { get; set; }
+    [StringLength(10)] public string? HouseNumber { get; set; }
 
     //Navigation Properties
-    public string? UserId { get; init; }
+    [StringLength(100)] public string? UserId { get; init; }
     public User? User { get; init; }
 }
