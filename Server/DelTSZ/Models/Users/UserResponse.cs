3c719988d58f6022347071ae9fe01ac33be125df
kC0d3 <kC0d3@github.com>
2024-05-23 09:12:50 +0200

    feat: Add UserResponse model class

---
+++ Server/DelTSZ/Models/Users/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿using DelTSZ.Models.Addresses;
+using DelTSZ.Models.Ingredients;
+using DelTSZ.Models.Products;
+
+namespace DelTSZ.Models.Users;
+
+public class UserResponse
+{
+    public string? Username { get; init; }
+    public string? Companyname { get; init; }
+    public string? Role { get; init; }
+    public AddressResponse? Address { get; init; }
+    public IEnumerable<IngredientResponse>? Ingredients { get; init; }
+    public IEnumerable<ProductResponse>? Products { get; init; }
+}
