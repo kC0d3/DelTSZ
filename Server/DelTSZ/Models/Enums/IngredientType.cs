83733f1b95816273488342a071fad5fc1565e4fd
kC0d3 <kC0d3@github.com>
2024-05-23 09:09:19 +0200

    feat: Add IngredientTypeExtension static class and GetIngredientTypes method

--- Server/DelTSZ/Models/Enums/IngredientType.cs
+++ Server/DelTSZ/Models/Enums/IngredientType.cs
@@ -13,3 +13,17 @@ public enum IngredientType
     Radish,
     Tomato,
 }
+
+public static class IngredientTypeExtensions
+{
+    public static IEnumerable<EnumResponse> GetIngredientTypes()
+    {
+        return Enum.GetValues(typeof(IngredientType))
+            .Cast<IngredientType>()
+            .Select(i => new EnumResponse
+            {
+                Index = (int)i,
+                Value = i.ToString()
+            }).ToList();
+    }
+}
