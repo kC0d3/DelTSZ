b2f8e78e30c0e8512775857310641bc08099fa3d
kC0d3 <kC0d3@github.com>
2024-05-23 09:03:57 +0200

    style: Rename Received property to Packed

--- Server/DelTSZ/Models/Products/ProductResponse.cs
+++ Server/DelTSZ/Models/Products/ProductResponse.cs
@@ -6,6 +6,6 @@ public class ProductResponse
 {
     public int Id { get; init; }
     public ProductType Type { get; init; }
-    public DateTime Received { get; init; }
+    public DateTime Packed { get; init; }
     public int Amount { get; init; }
 }
