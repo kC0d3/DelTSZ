db21f357d61e8713727ceee68a4e2adefb678af2
kC0d3 <kC0d3@github.com>
2024-05-23 09:16:18 +0200

    feat: Add UserController and GetUser API endpoint

---
+++ Server/DelTSZ/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using DelTSZ.Models.Ingredients;
+using DelTSZ.Models.Users;
+using DelTSZ.Repositories.UserRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DelTSZ.Controllers;
+
+[Route("api/users")]
+[ApiController]
+public class UserController(IUserRepository userRepository) : ControllerBase
+{
+    [HttpGet, Authorize]
+    public async Task<ActionResult<UserResponse>> GetUser()
+    {
+        try
+        {
+            var id = HttpContext.User.Claims.FirstOrDefault(c => c.Type.Contains("identifier"))?.Value;
+            if (id == null)
+                return NotFound("User not found");
+
+            return Ok(await userRepository.GetUserById(id));
+        }
+        catch (Exception)
+        {
+            return NotFound("Error getting user.");
+        }
+    }
+}
