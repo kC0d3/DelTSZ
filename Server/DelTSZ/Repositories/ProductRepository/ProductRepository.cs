a1419b8955b21c85e2f0ecca189f7e15cc75e2c1
kC0d3 <kC0d3@github.com>
2024-05-29 12:20:31 +0200

    refactor: Change ProductUpdateByRequestAmount method implementation

--- Server/DelTSZ/Repositories/ProductRepository/ProductRepository.cs
+++ Server/DelTSZ/Repositories/ProductRepository/ProductRepository.cs
@@ -41,72 +41,26 @@ public class ProductRepository(
         return await dataContext.Products.FirstOrDefaultAsync(p => p.Id == id);
     }
 
-    public async Task ProductUpdateByRequestAmount(ProductType type, string id, int amount)
+    public async Task ProductUpdateByRequestAmount(ProductType type, string userId, int amount)
     {
-        var demandAmount = amount;
+        var remainingAmount = amount;
 
-        while (demandAmount > 0)
+        while (remainingAmount > 0)
         {
             var ownerProduct = await GetOwnerOldestProductByType(type);
+            var transferableAmount = Math.Min(ownerProduct!.Amount, remainingAmount);
 
-            if (ownerProduct!.Amount <= demandAmount)
+            await TransferProduct(type, userId, ownerProduct, transferableAmount);
+
+            remainingAmount -= transferableAmount;
+
+            if (ownerProduct.Amount == 0)
             {
-                demandAmount -= ownerProduct.Amount;
-                var userProduct = await GetProductByUserId_Type_PackedDate(type, id, ownerProduct.Packed);
-
-                if (userProduct == null)
-                {
-                    var ingredients =
-                        await productIngredientRepository.CreateProductIngredientsFromProductIngredients(ownerProduct,
-                            ownerProduct.Amount);
-
-                    await CreateProductToUser(new ProductRequest
-                    {
-                        Type = type,
-                        Amount = ownerProduct.Amount
-                    }, id, ownerProduct.Packed, ingredients);
-                }
-                else
-                {
-                    userProduct.Amount += ownerProduct.Amount;
-                    userProduct.Ingredients =
-                        await productIngredientRepository.UpgradeProductIngredientsFromProductIngredients(ownerProduct,
-                            demandAmount,
-                            userProduct);
-                    await UpdateProduct(userProduct);
-                }
-
                 await DeleteProduct(ownerProduct);
             }
             else
             {
-                var userProduct = await GetProductByUserId_Type_PackedDate(type, id, ownerProduct.Packed);
-
-                if (userProduct == null)
-                {
-                    var ingredients =
-                        await productIngredientRepository.CreateProductIngredientsFromProductIngredients(ownerProduct,
-                            demandAmount);
-
-                    await CreateProductToUser(new ProductRequest
-                    {
-                        Type = type,
-                        Amount = demandAmount
-                    }, id, ownerProduct.Packed, ingredients);
-                }
-                else
-                {
-                    userProduct.Amount += demandAmount;
-                    userProduct.Ingredients =
-                        await productIngredientRepository.UpgradeProductIngredientsFromProductIngredients(ownerProduct,
-                            demandAmount,
-                            userProduct);
-                    await UpdateProduct(userProduct);
-                }
-
-                ownerProduct.Amount -= demandAmount;
                 await UpdateProduct(ownerProduct);
-                demandAmount = 0;
             }
         }
     }
@@ -139,6 +93,32 @@ public class ProductRepository(
 
     //Private methods
 
+    private async Task TransferProduct(ProductType type, string userId, Product ownerProduct, int amount)
+    {
+        var userProduct = await GetProductByUserId_Type_PackedDate(type, userId, ownerProduct.Packed);
+
+        if (userProduct == null)
+        {
+            var ingredients = await productIngredientRepository.CreateProductIngredientsFromProductIngredients(
+                ownerProduct, amount);
+
+            await CreateProductToUser(new ProductRequest
+            {
+                Type = type,
+                Amount = amount
+            }, userId, ownerProduct.Packed, ingredients);
+        }
+        else
+        {
+            userProduct.Amount += amount;
+            userProduct.Ingredients = await productIngredientRepository.UpgradeProductIngredientsFromProductIngredients(
+                ownerProduct, amount, userProduct);
+            await UpdateProduct(userProduct);
+        }
+
+        ownerProduct.Amount -= amount;
+    }
+    
     private async Task<Product?> GetOwnerOldestProductByType(ProductType type)
     {
         var owner = await userRepository.GetOwner();
