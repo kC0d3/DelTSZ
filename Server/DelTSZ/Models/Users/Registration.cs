e8b14a744c7c50fd9a7ac359f19fe6426c4443f5
kC0d3 <kC0d3@github.com>
2024-05-29 11:52:55 +0200

    refactor: Change Registration model record to class

--- Server/DelTSZ/Models/Users/Registration.cs
+++ Server/DelTSZ/Models/Users/Registration.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 using DelTSZ.Models.Addresses;
 
 namespace DelTSZ.Models.Users;
 
-public record Registration(
-    string Email,
-    string Username,
-    string Companyname,
-    AddressRequest Address,
-    string Password
-);
+public class Registration
+{
+    public string? Email { get; init; }
+    public string? UserName { get; init; }
+    public string? CompanyName { get; init; }
+    public AddressRequest? Address { get; init; }
+    public string? Password { get; init; }
+    
+    [Compare("Password", ErrorMessage = "Confirm password does not match.")]
+    public string? ConfirmPassword { get; init; }
+}
