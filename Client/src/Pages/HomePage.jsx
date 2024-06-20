459f953b5f31fe044e528e932b572d9940d00039
kC0d3 <kC0d3@github.com>
2024-06-20 11:15:01 +0200

    feat: Add HomeBody component to HomePage sheet

--- Client/src/Pages/HomePage.jsx
+++ Client/src/Pages/HomePage.jsx
@@ -1,10 +1,13 @@
-import Footer from "../Components/Footer";
-import Header from "../Components/Header";
+import Footer from '../Components/Footer';
+import Header from '../Components/Header';
+import Body from '../Components/HomeBody';
 
-export default function HomePage({ loggedUser, setLoggedUser, slides, interval }) {
+export default function HomePage({ loggedUser, setLoggedUser, slides, slidesInterval, achievementCounters, achievementDuration, achievementStart }) {
+
     return (
         <div className="homepage">
-            <Header {...{ loggedUser, setLoggedUser, slides, interval }} />
+            <Header {...{ loggedUser, setLoggedUser, slides, slidesInterval }} />
+            <Body {...{ achievementCounters, achievementDuration, achievementStart }} />
             <Footer />
         </div>
     );
