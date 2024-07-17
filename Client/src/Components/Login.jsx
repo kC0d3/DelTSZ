6cb689f4016dff0b100c7bdcbad88c8cd3c45dc0
kC0d3 <kC0d3@github.com>
2024-07-17 10:36:23 +0200

    feat: Add handleLogin function and typo fix

--- Client/src/Components/Login.jsx
+++ Client/src/Components/Login.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 
-export default function Login({ setLoggedUser, showLogin, setShowLogin, setShowRegistration }) {
-    const [login, setLogin] = useState({ username: '', password: '' });
+export default function Login({ fetchUser, showLogin, setShowLogin, setShowRegistration }) {
+    const [login, setLogin] = useState({ email: '', password: '' });
 
     const handleOverlayClick = (e) => {
         if (e.target.classList.contains('login')) {
@@ -18,19 +19,80 @@ export default function Login({ setLoggedUser, showLogin, setShowLogin, setShowR
         setShowLogin(false);
     }
 
+    const handleLogin = async e => {
+        e.preventDefault();
+
+        try {
+            const loginRes = await fetch('/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email: login.email, password: login.password })
+            });
+
+            const loginData = await loginRes.json();
+
+            if (loginRes.ok) {
+                setShowLogin(false);
+                fetchUser();
+                setTimeout(() => {
+                    setLogin({ email: '', password: '' });
+                }, 1000);
+
+                toast.success(loginData.message, {
+                    position: "top-right",
+                    autoClose: 3000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                });
+            }
+            else if (loginRes.status === 401) {
+                toast.warn(loginData.message, {
+                    position: "top-right",
+                    autoClose: 4000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                });
+            }
+            else {
+                toast.error(loginData.message, {
+                    position: "top-right",
+                    autoClose: 3000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return showLogin ?
         (<div className={'login active'} onClick={handleOverlayClick}>
             <div className={'login-form active'}>
-                <button className='login-close' onClick={handleClose}>&times;</button>
+                <button className='login-close-button' onClick={handleClose}>&times;</button>
                 <div className='login-header'>
                     <h1>Login</h1>
                 </div>
                 <div className='login-body'>
-                    <input type="text" placeholder="Email" value={login.username} onChange={e => setLogin({ ...login, username: e.target.value })} />
+                    <input type="text" placeholder="Email" value={login.email} onChange={e => setLogin({ ...login, email: e.target.value })} />
                     <input type="password" placeholder="Password" value={login.password} onChange={(e) => setLogin({ ...login, password: e.target.value })} />
                 </div>
                 <div className='login-footer'>
-                    <button className='login-button'>Login</button>
+                    <button className='login-button' onClick={handleLogin}>Login</button>
                     <button className='registration-button' onClick={handleShowRegister}>Registration</button>
                 </div>
             </div>
@@ -38,16 +100,16 @@ export default function Login({ setLoggedUser, showLogin, setShowLogin, setShowR
         :
         (<div className={'login'} onClick={handleOverlayClick}>
             <div className={'login-form'}>
-                <button className='login-close' onClick={handleClose}>x</button>
+                <button className='login-close-button' onClick={handleClose}>x</button>
                 <div className='login-header'>
                     <h1>Login</h1>
                 </div>
                 <div className='login-body'>
-                    <input type="text" placeholder="Email" value={login.username} onChange={e => setLogin({ ...login, username: e.target.value })} />
+                    <input type="text" placeholder="Email" value={login.email} onChange={e => setLogin({ ...login, email: e.target.value })} />
                     <input type="password" placeholder="Password" value={login.password} onChange={(e) => setLogin({ ...login, password: e.target.value })} />
                 </div>
                 <div className='login-footer'>
-                    <button className='login-button'>Login</button>
+                    <button className='login-button' onClick={handleLogin}>Login</button>
                     <button className='registration-button' onClick={handleShowRegister}>Registration</button>
                 </div>
             </div>
